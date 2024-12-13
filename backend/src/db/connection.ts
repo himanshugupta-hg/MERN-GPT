1430c3f4df39dae347b1e94cbe0d213187b8c7aa
himanshu-hg-github <21051821@kiit.ac.in>
2024-12-13 07:18:30 +0530

    hid mongo cred and moved .env

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,15 +1,11 @@
 import { connect, disconnect } from "mongoose";
-import { config } from "dotenv";
-
-config();
 
 async function connectToDatabase() {
   try {
-    console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URL);
+    await connect(process.env.MONGODB_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB:", error);
-    console.log(error);
     throw new Error("Could not Connect To MongoDB");
   }
 }
